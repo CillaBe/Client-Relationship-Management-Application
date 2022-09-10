60663ddee32f88d6d2361c2a5d5771bc611afd6e
CillaBe <priscilla.behen@gmail.com>
2022-09-10 12:51:26 -0600

    Wrote a funtion to sum appt month and type, need to create a code to populate months in dropdown and convert months to numbers

--- src/Controllers/Reports.java
+++ src/Controllers/Reports.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Helper.JDBC;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,11 +8,20 @@ import javafx.scene.control.*;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.MouseEvent;
 import model.Appointment;
+import model.Contact;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class Reports implements Initializable {
+    @FXML
+    private TextField TotalTextBox;
+    @FXML
+    private Button TotalButton;
     @FXML
     private TextField TotalTypeTextBox;
     @FXML
@@ -38,6 +48,32 @@ public class Reports implements Initializable {
         AppointmentTypeComboBox.setItems(Appointment.getApptTypes());
 
     }
+    /** Function to sum total appointments by month and type*/
+
+    public int SumTypeAndMonth (String type, int month) throws SQLException {
+        Connection connection = JDBC.openConnection();
+        int total = 0;
+
+        try {
+            String statement = "SELECT count(*) as total  FROM appointments Where extract(month from start) = ? and type = ?";
+
+            PreparedStatement ps = JDBC.openConnection().prepareStatement(statement);
+            ps.setInt(1, month);
+            ps.setString(2, type);
+            System.out.println(" Statement I'm sending to SQL to get sum of month and type appts" + ps + " ");
+            ResultSet rs = ps.executeQuery(statement);
+            while (rs.next()) {
+                 total = rs.getInt("total");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Error summing appts and typs");
+
+
+        }
+        return total;
+    }
+
 
     public void onReportsContactTab(ActionEvent actionEvent) {
     }
@@ -82,5 +118,11 @@ public class Reports implements Initializable {
     }
 
     public void onTotalMonth(ActionEvent actionEvent) {
+    }
+
+    public void onTotalTextBox(ActionEvent actionEvent) {
+    }
+
+    public void onTotalButton(ActionEvent actionEvent) {
     }
 }
